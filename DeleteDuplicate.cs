c6de1abf3a7bdb29b932241c56ae74d1f54c396b
Vighnahar <85437936+vighnaharsn@users.noreply.github.com>
2023-02-08 19:51:15 +0530

    Add files via upload

---
+++ DeleteDuplicate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteDuplicate
+{
+    /*
+     Logic : 
+    
+       TC: O(n)
+       SC: O(n)
+
+     */
+    internal class DeleteDuplicate
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddLast(4);
+            list.AddLast(3);
+            list.AddLast(2);
+            list.AddLast(2);
+            list.AddLast(1);
+            list.AddLast(3);
+            list.AddLast(2);
+            list.AddLast(5);
+            list.AddLast(4);            
+
+            LinkedListNode<int> head = list.First;
+
+            DelDuplciate(head);
+
+            Console.ReadKey();
+        }
+
+        static void DelDuplciate(LinkedListNode<int> head)
+        {
+            Dictionary<int, int> hm = new Dictionary<int, int>();
+
+            LinkedListNode<int> curr = head;
+
+            /* to remove duplictae value 
+            Dictionary<int, bool> hm = new Dictionary<int, bool>();
+
+            while (curr != null)
+            {
+                if (!(hm.ContainsKey(curr.Value)))
+                {
+                    Console.Write(curr.Value + " ");
+                    hm.Add(curr.Value, true);
+                }
+
+                curr = curr.Next;
+            }
+            */
+
+            /* to remove all duplictae */
+            while (curr != null)
+            {
+                if (!(hm.ContainsKey(curr.Value)))
+                {
+                    hm.Add(curr.Value, 1);
+                }
+                else
+                {
+                    hm[curr.Value] += 1;
+                }
+                curr = curr.Next;
+            }
+
+            foreach(var a in hm)
+            {
+                if(a.Value == 1)
+                {
+                    Console.Write(a.Key + " ");
+                }
+            }            
+        }
+    }
+}
---
+++ MergeTwoList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoList
+{
+    internal class MergeTwoList
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> listA = new LinkedList<int>();
+
+            listA.AddLast(1);
+            listA.AddLast(2);
+            listA.AddLast(5);          
+
+            LinkedListNode<int> headA = listA.First;
+
+            LinkedList<int> listB = new LinkedList<int>();
+
+            listB.AddLast(2);
+            listB.AddLast(3);
+            listB.AddLast(4);
+
+            LinkedListNode<int> headB = listB.First;
+
+            MrgTwoLst(headA, headB);
+            
+            Console.ReadKey();
+        }
+
+        static void MrgTwoLst(LinkedListNode<int> headA, LinkedListNode<int> headB)
+        {
+            if (headA == null)
+            {
+                while(headB != null)
+                {
+                    Console.Write(headB.Value + " ");
+                    headB = headB.Next;
+                }
+                return;
+            }
+            if (headB == null)
+            {
+                while (headA != null)
+                {
+                    Console.Write(headA.Value + " ");
+                    headA = headA.Next;
+                }
+                return;
+            }           
+
+            while (true)
+            {
+                if (headA != null && headB != null)
+                {
+                    if (headA.Value < headB.Value)
+                    {
+                        Console.Write(headA.Value + " ");
+                        headA = headA.Next;
+                    }
+                    else
+                    {
+                        Console.Write(headB.Value + " ");
+                        headB = headB.Next;
+                    }
+                }
+                else
+                if (headA != null && headB == null)
+                {
+                    Console.Write(headA.Value);
+                    headA = headA.Next;
+                }
+                else
+                if (headA == null && headB != null)
+                {
+                    Console.Write(headB.Value);
+                    headB = headB.Next;
+                }                
+            }           
+        }
+    }
+}
